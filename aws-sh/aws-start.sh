f058d94517c30df5d360975e27babf538691dfea
syokenz <syokenz@gmail.com>
2013-02-06 21:08:52 +0900

    node別スクリプト作成

--- aws-sh/aws-start.sh
+++ aws-sh/aws-start.sh
@@ -1,1 +1,1 @@
-/opt/bitnami/ruby/bin/ruby /root/git/lightweight_etc/aws-sh/ec2ctl.rb start | mail -s "Start AWS" 38-ossc-lwrandd@nri.co.jp
+/opt/bitnami/ruby/bin/ruby /root/git/lightweight_etc/aws-sh/ec2ctl_all.rb start | mail -s "Start AWS" 38-ossc-lwrandd@nri.co.jp
--- aws-sh/ec2ctl.rb
+++ aws-sh/ec2ctl.rb
@@ -2,15 +2,39 @@
 # encoding: utf-8
 require 'aws-sdk'
 
+NODE=["apache22", "apache24", "nginx", "node", "jboss", "mongo", "oracle", "client1", "gfc"]
+instancelist = [
+                "i-2393d020", #Apache2.2 server
+                "i-8193d082", #Apache2.4 server
+                "i-71eaa772", #Nginx server
+                "i-b3d796b0", #Node.js server
+                "i-13aed410", #JBoss server
+                "i-3f11683c", #MongoDB server
+                "i-11c0bb12", #Oracle server
+                "i-61dfa462", #client1 server
+                "i-45570a46" #GrowthForecast
+               ]
+nodehash = Hash.new
+NODE.each_with_index do |n,i|
+  nodehash[n] = instancelist[i]
+end
+
 COMMAND=["start", "stop", "status"]
 
-unless ARGV.size == 1
-  puts "Usage: #{$0} <command(#{COMMAND.join('/')})>"
+unless ARGV.size == 2
+  puts "Usage: #{$0} <{#{NODE.join('|')}}> <command(#{COMMAND.join('|')})>"
   puts ""
   exit 1
 end
 
-command = ARGV[0]
+node = ARGV[0]
+command = ARGV[1]
+
+unless NODE.index(node)
+  puts "node must be #{NODE.join(' or ')} !"
+  puts ""
+  exit 1
+end
 
 unless COMMAND.index(command)
   puts "command must be #{COMMAND.join(' or ')} !"
@@ -18,30 +42,17 @@ unless COMMAND.index(command)
   exit 1
 end
  
-instancelist = ["i-45570a46", #GrowthForecast
-                "i-3f11683c", #MongoDB server
-                "i-11c0bb12", #Oracle server
-                "i-13aed410", #JBoss server
-                "i-b3d796b0", #Node.js server
-                "i-2393d020", #Apache2.2 server
-                "i-8193d082", #Apache2.4 server
-                "i-71eaa772", #Nginx server
-                "i-61dfa462", #client1 server
-               ]
- 
 AWS.config(YAML.load(File.read("/etc/aws.yml")))
  
-# start instance
-instancelist.each do |ins|
-  i = AWS::EC2.new.instances[ins]
-  if command == 'start' && i.status == :stopped
-    puts "start #{i.tags['Name']}"
-    i.start
-  elsif command == 'stop' && i.status == :running
-    puts "stop #{i.tags['Name']}"
-    i.stop
-  elsif command == 'status'
-    puts "#{i.tags['Name']} is #{i.status}"
-  end
+
+i = AWS::EC2.new.instances[nodehash[node]]
+if command == 'start' && i.status == :stopped
+  puts "start #{i.tags['Name']}"
+  i.start
+elsif command == 'stop' && i.status == :running
+  puts "stop #{i.tags['Name']}"
+  i.stop
+elsif command == 'status'
+  puts "#{i.tags['Name']} is #{i.status}"
 end
  
