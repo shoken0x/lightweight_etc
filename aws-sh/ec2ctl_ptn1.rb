3ae7cfef3852e36ec200aa098ad8fb4649fa4634
a-hayashida <a-hayashida@nri.co.jp>
2013-02-12 09:17:37 +0000

    パターン別AWS起動／再起動モジュール作成

---
+++ aws-sh/ec2ctl_ptn1.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+require 'aws-sdk'
+
+COMMAND=["start", "stop", "status"]
+
+unless ARGV.size == 1
+  puts "Usage: #{$0} <command(#{COMMAND.join('/')})>"
+  puts ""
+  exit 1
+end
+
+command = ARGV[0]
+
+unless COMMAND.index(command)
+  puts "command must be #{COMMAND.join(' or ')} !"
+  puts ""
+  exit 1
+end
+ 
+instancelist = ["i-11c0bb12", #Oracle server
+                "i-13aed410", #JBoss server
+                "i-2393d020", #Apache2.2 server
+               ]
+ 
+AWS.config(YAML.load(File.read("/etc/aws.yml")))
+ 
+# start instance
+instancelist.each do |ins|
+  i = AWS::EC2.new.instances[ins]
+  if command == 'start' && i.status == :stopped
+    puts "start #{i.tags['Name']}"
+    i.start
+  elsif command == 'stop' && i.status == :running
+    puts "stop #{i.tags['Name']}"
+    i.stop
+  elsif command == 'status'
+    puts "#{i.tags['Name']} is #{i.status}"
+  end
+end
+ 
